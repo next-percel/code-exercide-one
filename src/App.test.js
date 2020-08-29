e0a6fc1d3ca6967797845f80c2efd07227330dad
Milind Nayse <milindnayse@gmail.com>
2020-08-29 23:35:20 +0200

    Initial project setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', ()=> {
+  const wrapper = shallow(<App />);
+  it('should display header as Tic Tac Toe', ()=> {
+    expect(wrapper.find('h1').text()).toBe('Tic Tac Toe');
+  });
 });
