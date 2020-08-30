f273c1668e606b03b378cc56d6366c07c03c7430
Milind Nayse <milindnayse@gmail.com>
2020-08-30 20:17:04 +0200

    Refactor the win logic

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,26 +1,19 @@
 export const calculateWinner = board => {
-  if (board[0] && board[0] === board[1] && board[0] === board[2]) {
-    return board[0]
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
   }
-  if (board[3] && board[3] === board[4] && board[3] === board[5]) {
-    return board[3]
-  }
-  if (board[6] && board[6] === board[7] && board[6] === board[8]) {
-    return board[6]
-  }
-  if (board[0] && board[0] === board[3] && board[0] === board[6]) {
-    return board[0]
-  }
-  if (board[1] && board[1] === board[4] && board[1] === board[7]) {
-    return board[1]
-  }
-  if (board[2] && board[2] === board[5] && board[2] === board[8]) {
-    return board[2]
-  }
-  if (board[0] && board[0] === board[4] && board[0] === board[8]) {
-    return board[0]
-  }
-  if (board[2] && board[2] === board[4] && board[2] === board[6]) {
-    return board[2]
-  }
+  return null;
 }
